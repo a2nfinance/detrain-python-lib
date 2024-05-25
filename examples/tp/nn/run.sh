7b7dc851b831ed59a372b1d0ea2de6a416ce2d5a
a2nfinance <levi@a2n.finance>
2024-05-25 09:12:43 +0700

    remove example

--- examples/tp/nn/run.sh
+++
@@ -1,2 +0,0 @@
-echo "Launching TP training"
-torchrun --nnodes=2 --nproc_per_node=1 --node_rank=0 --rdzv_id=101 --rdzv_endpoint="localhost:9999" main.py --gpu="0_0_0" --epochs=4 --batch_size=50 --lr=0.001 --model_name="tp_01"
